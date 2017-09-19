d4f81d795dd3288f0e26a0ef6da10595d9faeccf
nirLuria <2nirluria@gmail.com>
2017-09-19 19:53:49 +0300

    added photo to friend - not yet in db.

--- ContainerChoose.java
+++ ContainerChoose.java
@@ -1,10 +1,24 @@
 package com.maybethem.maybethem;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 /**
  * Created by nirlu on 10/09/2017.
@@ -15,6 +29,7 @@ public class ContainerChoose extends AppCompatActivity
 
     Button go_to_conteiner_men, go_to_conteiner_women;
 
+
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -22,8 +37,8 @@ public class ContainerChoose extends AppCompatActivity
 
         goToConteinerMenClickListener();
         goToConteinerWomenClickListener();
-    }
 
+    }
 
 
     public void goToConteinerMenClickListener()
--- Details.java
+++ Details.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,6 +14,7 @@ import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -58,6 +61,12 @@ public class Details extends AppCompatActivity
     String otherRedLine, redLineItems;
     EditText inputRedLine;
 
+    //image variables.
+    ImageView imageView;
+    Button btnChoose;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
+
 
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -75,16 +84,42 @@ public class Details extends AppCompatActivity
         mRedLine = (Button)findViewById(R.id.btnRedLine);
 
 
+        btnChoose=(Button)findViewById(R.id.btnChoose);
+        imageView=(ImageView)findViewById(R.id.imageView);
 
+
+
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
 
 
         redLineOnClickListener();
         hobbiesOnClickListener();
         submit();
-     ///   addNewTask();
-
     }
 
+
+    public void openGallery()
+    {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode==RESULT_OK && requestCode==PICK_IMAGE)
+        {
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+
+    }
 
     public void addOtherHobbies()
     {
